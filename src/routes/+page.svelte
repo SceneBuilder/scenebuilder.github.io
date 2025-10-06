0e28563dbda4fbf4b43a3e9cb783f5ace5e1d502
Yunho Cho <ycho358@gatech.edu>
2025-10-06 13:41:12 -0400

    Made single room demo suitable for multiple

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,19 @@
 	import Abstract from '$lib/content/abstract.md';
 	import { Canvas } from '@threlte/core';
 	import Scene from '$lib/components/Scene.svelte';
+
+	const scenes = [
+		{
+			title: 'Classroom',
+			url: '/scenes/single_room/classroom.glb',
+			// extra_info: TODO
+		},
+		{
+			title: 'Bar',
+			url: '/scenes/single_room/bar.glb',
+			// extra_info: TODO
+		},
+	];
 </script>
 
 <main class="grid grid-cols-[1fr,min(80ch,100%),1fr] gap-y-8 py-8">
@@ -31,14 +44,16 @@
 		<section class="space-y-6">
 			<h2 class="text-center text-2xl font-bold tracking-tighter sm:text-3xl">Examples</h2>
 			<div class="mx-auto max-w-6xl space-y-8">
-				<div class="space-y-4">
-					<h3 class="text-center text-xl font-semibold">Classroom</h3>
-					<div class="mx-auto h-96 w-full max-w-4xl rounded-lg border bg-background">
-						<Canvas>
-							<Scene url="/scenes/single_room/classroom.glb" />
-						</Canvas>
+				{#each scenes as scene}
+					<div class="space-y-4">
+						<h3 class="text-center text-xl font-semibold">{scene.title}</h3>
+						<div class="mx-auto h-96 w-full max-w-4xl rounded-lg border bg-background">
+							<Canvas>
+								<Scene url={scene.url} />
+							</Canvas>
+						</div>
 					</div>
-				</div>
+				{/each}
 			</div>
 		</section>
 	</div>
