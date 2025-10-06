cffe1747134a5b408a323a498e0f019f2ab54609
Yunho Cho <ycho358@gatech.edu>
2025-10-06 07:23:52 -0400

    Added `threlte` to project

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
+</script>
+
+<T.PerspectiveCamera makeDefault position={[10, 10, 10]}>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight castShadow position={[3, 10, 10]} />
+<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+<T.AmbientLight intensity={0.2} />
+
+<T.Mesh>
+  <T.BoxGeometry />
+  <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
